bd5326e854dd4ad0d5e2689866372169ea9811bd
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2026-01-20 21:04:22 +0200

    8

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      Components: path.resolve(__dirname, "./src/Components"),
+      components: path.resolve(__dirname, "./src/components"),
     },
   },
   define: {
