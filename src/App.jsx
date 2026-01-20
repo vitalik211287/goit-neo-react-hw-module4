dd4622212550421c80c328581336ca751d6f151e
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2026-01-20 20:13:17 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,3 @@
-// import React from "react";
-// import SearchBar from "components/SearchBar/SearchBar.jsx";
-// import ImageGallery from "components/ImageGallery/ImageGallery.jsx";
-// import LoadMoreBtn from "components/LoadMoreBtn/LoadMoreBtn.jsx";
-// import { useState } from "react";
-// import { unsplashApi } from "components/api/api";
-// import "./App.css";
-
-// function App() {
-//   const onSubmit = async (query) => {
-//     try {
-//       setSearchQuery(query); // запам’ятали що шукаємо
-
-//       const res = await unsplashApi.get("/search/photos", {
-//         params: { query, page: 1, per_page: 12 },
-//       });
-
-//       setImages(res.data.results); // ← ОЦЕ КЛЮЧОВЕ
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-
-//   const loadMoreImg = async () => {
-//     // setSearchQuery("");
-//     try {
-//       const nextPage = Math.floor(images.length / 12) + 1;
-
-//       const res = await unsplashApi.get("/search/photos", {
-//         params: { query: searchQuery, page: nextPage, per_page: 12 },
-//       });
-
-//       setImages((prevImages) => [...prevImages, ...res.data.results]);
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   }
-
-//   const [images, setImages] = useState([]);
-//   const [searchQuery, setSearchQuery] = useState("");
-
-//   console.log(images);
-
-//   return (
-//     <>
-//       <SearchBar
-//         onSubmit={onSubmit}
-//         searchQuery={searchQuery}
-//         setSearchQuery={setSearchQuery}
-//       />
-//       <ImageGallery images={images} />
-//       <>{images.length > 0 && <LoadMoreBtn loadMoreImg={loadMoreImg} />}</>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import { useState } from "react";
 import SearchBar from "components/SearchBar/SearchBar.jsx";
 import ImageGallery from "components/ImageGallery/ImageGallery.jsx";
@@ -72,12 +14,8 @@ function App() {
   const [images, setImages] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [page, setPage] = useState(1);
-
   const [totalPages, setTotalPages] = useState(0);
-
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
   const [selectedImage, setSelectedImage] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -101,16 +39,13 @@ function App() {
 
     try {
       setIsLoading(true);
-      setError(null);
 
       // скидання попереднього пошуку
       setSearchQuery(normalized);
       setPage(1);
       setImages([]);
-      // setTotalResults(0);
       setTotalPages(0);
 
-      // прибираємо старі тости типу "це всі результати"
       toast.dismiss("end");
 
       const res = await unsplashApi.get("/search/photos", {
@@ -134,7 +69,6 @@ function App() {
         toast("Це всі результати ✅", { id: "end", icon: "✅" });
       }
     } catch (err) {
-      setError(err);
       toast.error("Помилка запиту 😢");
       console.error(err);
     } finally {
@@ -155,7 +89,6 @@ function App() {
 
     try {
       setIsLoading(true);
-      setError(null);
 
       const nextPage = page + 1;
 
@@ -180,7 +113,6 @@ function App() {
         toast("Це всі результати ✅", { id: "end", icon: "✅" });
       }
     } catch (err) {
-      setError(err);
       toast.error("Не вдалося завантажити ще 😢");
       console.error(err);
     } finally {
@@ -195,12 +127,6 @@ function App() {
       <Toaster position="top-right" />
 
       <SearchBar onSubmit={onSubmit} />
-{/* 
-      {error && (
-        <p style={{ textAlign: "center" }}>
-          Сталася помилка запиту. Дивись console.
-        </p>
-      )} */}
 
       <ImageGallery images={images} onImageClick={openModal} />
 
@@ -220,4 +146,3 @@ function App() {
 }
 
 export default App;
-
--- src/Components/ImageGallery/ImageGallery.jsx
+++ src/Components/ImageGallery/ImageGallery.jsx
@@ -1,20 +1,3 @@
-// import ImageCard from './ImageCard/ImageCard.jsx';
-// import css from './ImageGallery.module.css';
-// import "simplelightbox/dist/simple-lightbox.min.css";
-
-// function ImageGallery({ images }) {
-//     console.log(images);
-//     return (
-//         <ul className={css.cardList}>
-//             {images.map(image => (
-//                 <ImageCard key={image.id} alt_description={image.alt_description} urls={image.urls} />
-//             ))}
-//         </ul>
-//     )
-// }
-
-// export default ImageGallery
-
 import ImageCard from "./ImageCard/ImageCard.jsx";
 import css from "./ImageGallery.module.css";
 
